df0f411284106653aae52dc3892266fad75d726c
Tahmid Galib <galibtahmid@gmail.com>
2025-08-29 01:51:55 +0600

    commited

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let heartsInBox=document.getElementsByClassName('heart')
+let numberOfHearts=document.getElementById("heart-number")
+let count=0
 for(let heart of heartsInBox){
     heart.addEventListener('click', function(){
-        alert("clicked")
+        count++;
+        numberOfHearts.innerText=count
     })
 }
