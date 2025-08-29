45a66fbbaa188a3e5432aa768cce8208b37c33ee
Tahmid Galib <galibtahmid@gmail.com>
2025-08-29 15:44:56 +0600

    copy part done

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ for(let heart of heartsInBox){
         numberOfHearts.innerText=count
     })
 }
+
+let totalNumberOfCopies=document.getElementById("copyTotal")
+let copyButtons=document.getElementsByClassName("copy")
+let copyCount=0
+
+
+
+for(let btn of copyButtons){
+    btn.addEventListener('click', function(){
+        copyCount++
+        totalNumberOfCopies.innerText=copyCount
+
+        let parent=this.closest(".box")
+        let phoneNumber=parent.querySelector(".phn_num")
+        let toCopy=phoneNumber.innerText
+
+        let input=document.createElement("input")
+        input.value=toCopy
+        document.body.appendChild(input)
+
+        input.select()
+        document.execCommand("copy")
+        document.body.removeChild(input)
+        alert("Number copied")
+    })
+}
+
+
